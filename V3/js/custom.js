30f656f7cd728daf3ba51febef567b9a22d663e7
Bharaniravichandran <102538292+Bharaniravichandran@users.noreply.github.com>
2022-08-11 16:21:13 +0530

    Delete V3 directory

--- V3/js/custom.js
+++
@@ -1,9 +0,0 @@
-$('.dropdown').on('show.bs.dropdown', function(e){
-  $(this).find('.dropdown-menu').first().stop(true, true).fadeIn();
-});
-
-// Add slideUp animation to dropdown
-$('.dropdown').on('hide.bs.dropdown', function(e){
-  $(this).find('.dropdown-menu').first().stop(true, true).fadeOut();
-});
-
